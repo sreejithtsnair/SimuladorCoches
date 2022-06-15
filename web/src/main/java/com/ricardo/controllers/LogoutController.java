6cf67654ba3a5e3f337d82bbf1fa09b5392e6b21
ricardonetmind <ricardoag@expert.netmind.es>
2022-06-15 12:30:09 +0200

    login logout

---
+++ web/src/main/java/com/ricardo/controllers/LogoutController.java
@@ -0,0 +1,26 @@
+package com.ricardo.controllers;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet("/logout")
+public class LogoutController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+//        session.removeAttribute("is_logged");
+//        session.removeAttribute("email");
+        session.invalidate();
+        resp.sendRedirect(req.getContextPath() + "/login");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
+}
